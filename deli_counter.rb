06753ed2a717015f7315db0f2e6fe4d1f4b87179
Kelsey Low <kklowlow@gmail.com>
2019-01-21 19:41:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
-    message = "The line is currently: "
+    message = "The line is currently:"
     katz_deli.each_with_index do |name, number|
-      message += "#{number.to_i + 1}. #{name}"
+      message += " #{number.to_i + 1}. #{name}"
     end
     puts "#{message}"
   end
