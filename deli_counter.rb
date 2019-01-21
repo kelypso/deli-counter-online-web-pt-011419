65e642038c47f930e7d1cc2a820c95a04ad7b93b
Kelsey Low <kklowlow@gmail.com>
2019-01-21 19:42:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name 
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  katz_deli << name
+    
 end
 
 def now_serving(line)
